0b8b86d11c277fe7c9de399f9a7c25011f3e35aa
xkattanr <xkattanr@fi.muni.cz>
2024-08-27 21:05:07 +0200

    some progress

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,47 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
 
+const brackets = ["<", ">"]
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const rawTemplates: string = core.getInput("templates")
+    const rawVars: string = core.getInput("vars")
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Templates: ${rawTemplates}`)
+    core.debug(`Vars: ${rawVars}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+function getOutputs(rawTemplates: string, rawVars: string): string[] {
+  const templates: string[] = JSON.parse(rawTemplates)
+  const vars: { [key: string]: string } = JSON.parse(rawVars)
+
+  templates.forEach((template) => {
+
+  })
+
+  return [""]
+}
+
+function sub(template: string, vars: { [key: string]: string }): string {
+  let stack: string[] = []
+  let load: boolean = false
+  for (const c of template) {
+    if (brackets.includes(c)) {
+      load = !load
+      continue
+    }
+
+
+  }
+  return ""
 }
